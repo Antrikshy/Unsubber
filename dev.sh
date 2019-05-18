b22f59ef3aa1d1aff4baf5e765b7b893f4c077c2
Antriksh Yadav <antrikshy@gmail.com>
2019-05-18 00:00:17 -0700

    Made progress on login, templates, stubbed other endpoints

--- dev.sh
+++ dev.sh
@@ -2,4 +2,7 @@
 
 export FLASK_APP=src/unsubber.py
 export FLASK_ENV=development
+export UNSUBBER_CLIENT_ID=$(head -n 1 config.txt)
+export UNSUBBER_CLIENT_SECRET=$(head -n 2 config.txt | tail -n 1)
+
 flask run
--- src/unsubber.py
+++ src/unsubber.py
@@ -1,12 +1,15 @@
-from flask import Flask, request, redirect
+import os
+
+from flask import Flask, request, render_template, make_response, redirect
 import requests
+
 
 app = Flask(__name__)
 
 _REDDIT_AUTH_URL_TEMPLATE = 'https://www.reddit.com/api/v1/authorize?client_id={}&response_type={}&state={}&redirect_uri={}&duration={}&scope={}'
-_APP_CLIENT_ID = ''
+_APP_CLIENT_ID = os.environ['UNSUBBER_CLIENT_ID']
 # TODO: Remove this from app ^^^
-_APP_CLIENT_SECRET = ''
+_APP_CLIENT_SECRET = os.environ['UNSUBBER_CLIENT_SECRET']
 # TODO: Remove this from app ^^^
 _APP_REDIRECT_URL = 'http://127.0.0.1:5000/oauth-redirect'
 # TODO: Remove this from app ^^^
@@ -28,9 +31,25 @@ def _generate_reddit_auth_code_payload(code):
         'redirect_uri': _APP_REDIRECT_URL
     }
 
+
+# ------------------------------------------
+# -------- PAGE RENDERING AND LOGIN --------
+# ------------------------------------------
+
+@app.route('/')
+def render_login():
+    return render_template('login.html')
+
 @app.route('/login')
-def hello_world():
+def redirect_to_login():
     return redirect(_generate_reddit_oauth_url())
+
+@app.route('/unsub')
+def render_main_app():
+    token = request.cookies.get('access_token')
+    if token is None:
+        return redirect('/')
+    return render_template('main.html')
 
 @app.route('/oauth-redirect')
 def handle_redirect():
@@ -39,6 +58,26 @@ def handle_redirect():
     auth_res = requests.post('https://www.reddit.com/api/v1/access_token',
         auth=requests.auth.HTTPBasicAuth(_APP_CLIENT_ID, _APP_CLIENT_SECRET),
         data=_generate_reddit_auth_code_payload(code),
-        headers={'User-Agent': 'Unsubber (in-development) by u/Antrikshy'}
+        headers={'User-Agent': 'Unsubber by u/Antrikshy'}
     )
-    return auth_res.json()['access_token']
+    token = auth_res.json()['access_token']
+    res = make_response(redirect('/unsub'))
+    res.set_cookie('access_token', token)
+    return res
+
+
+# ------------------------------------------
+# ------------- MAIN ENDPOINTS -------------
+# ------------------------------------------
+
+@app.route('/my-subreddits')
+def get_subreddits():
+    pass
+
+@app.route('/get-subreddit-state')
+def get_subreddit_state():
+    pass
+
+@app.route('/unsubscribe')
+def unsubscribe_from_subreddit():
+    pass
