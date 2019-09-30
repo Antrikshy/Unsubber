48413764b2299602eb06f76eb8db82b541f4f40d
Antriksh Yadav <antrikshy@gmail.com>
2019-09-29 23:43:32 -0700

    Early MVP, data flow w/ lazy loading working

--- src/unsubber.py
+++ src/unsubber.py
@@ -1,18 +1,23 @@
 import os
+import itertools
+import multiprocessing as mp
 
-from flask import Flask, request, render_template, make_response, redirect
+from flask import Flask, request, render_template, make_response, jsonify, redirect
 import requests
 
 
 app = Flask(__name__)
 
-_REDDIT_AUTH_URL_TEMPLATE = 'https://www.reddit.com/api/v1/authorize?client_id={}&response_type={}&state={}&redirect_uri={}&duration={}&scope={}'
+_REDDIT_ROOT_URL = 'https://www.reddit.com'
+_REDDIT_OAUTH_ROOT_URL = 'https://oauth.reddit.com'
+_REDDIT_AUTH_URL_TEMPLATE = _REDDIT_ROOT_URL + \
+    '/api/v1/authorize?client_id={}&response_type={}&state={}&redirect_uri={}&duration={}&scope={}'
 _APP_CLIENT_ID = os.environ['UNSUBBER_CLIENT_ID']
-# TODO: Remove this from app ^^^
 _APP_CLIENT_SECRET = os.environ['UNSUBBER_CLIENT_SECRET']
-# TODO: Remove this from app ^^^
 _APP_REDIRECT_URL = 'http://127.0.0.1:5000/oauth-redirect'
 # TODO: Remove this from app ^^^
+_APP_HTTP_REQUEST_HEADER = {'User-Agent': 'Unsubber (website; in-development) by u/Antrikshy'}
+
 
 def _generate_reddit_oauth_url():
     return _REDDIT_AUTH_URL_TEMPLATE.format(
@@ -24,12 +29,52 @@ def _generate_reddit_oauth_url():
         'mysubreddits,subscribe'
     )
 
+
 def _generate_reddit_auth_code_payload(code):
     return {
         'grant_type': 'authorization_code',
         'code': code,
         'redirect_uri': _APP_REDIRECT_URL
     }
+
+
+def _get_all_user_subreddits(access_token):
+    headers = _APP_HTTP_REQUEST_HEADER.copy()
+    headers['Authorization'] = 'Bearer ' + access_token
+    after = ''
+    subreddits = []
+    while after is not None:
+        url = _REDDIT_OAUTH_ROOT_URL + '/subreddits/mine/subscriber?raw_json=1&limit=100' + '&after=' + after
+        res = requests.get(url, headers=headers)
+        if res.status_code != 200:
+            res.raise_for_status()
+        j_res = res.json()
+        after = j_res['data']['after']
+        subreddits += [
+            {
+                'display_name': i['data']['display_name'],
+                'subscribers': i['data']['subscribers']
+            }
+            for i in j_res['data']['children']
+        ]
+    return subreddits
+
+
+def _is_subreddit_active(subreddit, access_token):
+    # Reading response
+    res = requests.get(f'{_REDDIT_ROOT_URL}/r/{subreddit}/new.json', headers=_APP_HTTP_REQUEST_HEADER)
+    if res.status_code != 200:
+        res.raise_for_status()
+    j_res = res.json()
+    # Reading subreddit and post metadata
+    r_subreddit_data = j_res['data']['children']
+    r_subreddit_subscribers = r_subreddit_data[0]['data']['subreddit_subscribers']
+    r_post_created_collation = list(map(lambda post: post['data']['created'], r_subreddit_data))
+    # Computing active state (aka #TheAlgorithm)
+    subreddit_is_active = True
+    if r_subreddit_subscribers < 200:
+        subreddit_is_active = False
+    return {'is_active': subreddit_is_active, 'subreddit': subreddit}
 
 
 # ------------------------------------------
@@ -40,9 +85,11 @@ def _generate_reddit_auth_code_payload(code):
 def render_login():
     return render_template('login.html')
 
+
 @app.route('/login')
 def redirect_to_login():
     return redirect(_generate_reddit_oauth_url())
+
 
 @app.route('/unsub')
 def render_main_app():
@@ -51,14 +98,15 @@ def render_main_app():
         return redirect('/')
     return render_template('main.html')
 
+
 @app.route('/oauth-redirect')
 def handle_redirect():
     code = request.args.get('code')
     state = request.args.get('state')
-    auth_res = requests.post('https://www.reddit.com/api/v1/access_token',
+    auth_res = requests.post(_REDDIT_ROOT_URL + '/api/v1/access_token',
         auth=requests.auth.HTTPBasicAuth(_APP_CLIENT_ID, _APP_CLIENT_SECRET),
         data=_generate_reddit_auth_code_payload(code),
-        headers={'User-Agent': 'Unsubber by u/Antrikshy'}
+        headers=_APP_HTTP_REQUEST_HEADER
     )
     token = auth_res.json()['access_token']
     res = make_response(redirect('/unsub'))
@@ -72,11 +120,28 @@ def handle_redirect():
 
 @app.route('/my-subreddits')
 def get_subreddits():
-    pass
+    token = request.cookies.get('access_token')
+    try:
+        subreddits = _get_all_user_subreddits(token)
+        return jsonify(subreddits=subreddits)
+    except requests.exceptions.HTTPError as e:
+        res = make_response()
+        res.status_code = e.status_code
+        return res
 
-@app.route('/get-subreddit-state')
-def get_subreddit_state():
-    pass
+
+@app.route('/get-subreddit-states')
+def get_subreddit_states():
+    token = request.cookies.get('access_token')
+    subreddits = request.args.getlist('subreddits[]')
+    with mp.Pool(5) as pool:
+        active_states = list(
+            pool.starmap(
+                _is_subreddit_active, 
+                zip(subreddits, itertools.repeat(token)))
+        )
+    return jsonify(active_states=active_states)
+
 
 @app.route('/unsubscribe')
 def unsubscribe_from_subreddit():
